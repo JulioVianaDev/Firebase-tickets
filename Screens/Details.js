598701b737162a62c2b625f88b5e8ff1c07b4476
JulioVianaDev <JulioVianaDev@gmail.com>
2023-06-17 06:13:49 -0300

    styles

--- Screens/Details.js
+++ Screens/Details.js
@@ -2,27 +2,28 @@ import { View, Text,TextInput,StyleSheet,Pressable } from 'react-native'
 import React,{useState} from 'react'
 import { updateChamado } from '../functions/updateChamado';
 import { useNavigation } from '@react-navigation/native';
+import { detailsStyles } from '../styles/Details';
 const Details = ({route}) => {
     const [nomeText,setNomeText] = useState(route.params.item.nome);
     const [ocorridoText,setOcorridoText] = useState(route.params.item.ocorrido);
     const id = route.params.item.id
     const navigation = useNavigation();
     return (
-        <View style={styles.container}>
+        <View style={detailsStyles.container}>
             <TextInput
-                style={styles.textField}
+                style={detailsStyles.textField}
                 onChangeText={setNomeText}
                 value={nomeText}
                 placeholder='Nome'
             />
             <TextInput
-                style={styles.textField}
+                style={detailsStyles.textField}
                 onChangeText={setOcorridoText}
                 placeholder='Descrição'
                 value={ocorridoText}
             />
             <Pressable
-                style={styles.buttonUpdate}
+                style={detailsStyles.buttonUpdate}
                 onPress={()=>updateChamado(nomeText,ocorridoText,id,navigation)}
             >
                 <Text>UPDATE</Text>
@@ -32,30 +33,3 @@ const Details = ({route}) => {
 }
 
 export default Details
-
-
-const styles = StyleSheet.create({
-    container:{
-        marginTop: 80,
-        marginLeft: 15,
-        marginRight: 15,
-    },
-    textField:{
-        marginBottom: 10,
-        padding:10,
-        fontSize:15,
-        color: '#000000',
-        backgroundColor: '#e0e0e0',
-        borderRadius: 5
-    },
-    buttonUpdate:{
-        marginTop:25,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 10,
-        backgroundColor: '#0de065'
-    }
-})
---
+++ styles/Details.js
@@ -0,0 +1,25 @@
+export const detailsStyles = StyleSheet.create({
+  container:{
+      marginTop: 80,
+      marginLeft: 15,
+      marginRight: 15,
+  },
+  textField:{
+      marginBottom: 10,
+      padding:10,
+      fontSize:15,
+      color: '#000000',
+      backgroundColor: '#e0e0e0',
+      borderRadius: 5
+  },
+  buttonUpdate:{
+      marginTop:25,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 10,
+      backgroundColor: '#0de065'
+  }
+})
